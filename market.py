6f158e9c73a98277aaf3b3c736ca5c0ca6e2c0c1
min02choi <min02choi@naver.com>
2023-05-28 18:18:23 +0900

    [PC:2023-05-28] Menu Select

--- market.py
+++ market.py
@@ -71,6 +71,7 @@ def enter_pw(user_cno):
             err_cnt += 1
 
 # 신규회원 가입
+# 반환 값 -> 없음
 def newuser_register():
     cur = con.cursor()
     cur.execute("SELECT cno FROM customer")
@@ -124,6 +125,118 @@ def newuser_register():
     print("회원가입이 완료되었습니다.")
     print("반갑습니다, ", newuser_name, "님!")
 
+# 가게의 메뉴를 보여주는 함수
+# 반환
+def show_menus():
+    cur = con.cursor()
+    cur.execute("SELECT pid, name, cate, price FROM product")
+    rows = cur.fetchall()
+
+    print("===================== < 메뉴 목록 > =====================")
+    print(" 메뉴번호 |      메뉴이름      |  카테고리  |   가격 ")
+    for row in rows:
+        print("{:>8}  | {:<16} {:^8} {:>10}".format(row['pid'], row['name'], row['cate'], row['price']))
+        # print(row['pid'], row['name'], row['cate'])
+    print("========================================================")
+
+# 고객이 물건을 선택하는 메소드
+# cart를 부름?
+# 주문 한 건을 완성시켜야 함
+def select_menu(user_cno):
+
+############ 에러나 ############################################
+
+    # order 테이블의 튜플을 우선 생성 
+    cur = con.cursor()
+    sql = "INSERT INTO orders(cus_no) VALUES (%s)"
+    cur.execute(sql, (str(user_cno)))
+    rows = cur.fetchall()
+
+########################################################
+    
+    # 가장 큰 oid가 현재 주문중인 oid임. 즉, order 테이블 중 가장 큰 oid를  선택
+    sql = "SELECT max(oid) FROM orders"
+    cur.execute(sql)
+    rows = cur.fetchall()
+    current_oid = rows[0]['max(oid)']          
+
+    # print(">>>>>>>>current_oid: ", current_oid)
+
+    total_menu_price = 0    # order에서 당기기?
+    continue_buy = 0        # 계속 구매
+    
+    while (continue_buy == 0):
+
+        cur = con.cursor()
+        cur.execute("SELECT pid FROM product")
+        rows = cur.fetchall()
+
+        print("메뉴를 선택해 주세요.")
+        show_menus()
+        input_pid = int(input(" 메뉴 선택 >> "))
+
+        # 입력한 번호가 상품 번호에 있는지
+        flag = 0
+        while (flag == 0):
+            for row in rows:
+                if (input_pid == row['pid']):
+                    flag = 1
+            if (flag == 0):
+                print("메뉴가 올바르지 않습니다. 다시 선택해주세요")
+                input_pid = int(input(" 메뉴 선택 >> "))
+        
+        # 메뉴의 수량 입력
+        # print("")
+        print("메뉴의 수량을 입력해주세요")
+        input_menu_qty = int(input(" 메뉴의 수량 >> "))
+
+        # 메뉴 이름, 가격 구하기
+        sql = "SELECT name, price FROM product WHERE pid = %s"
+        cur.execute(sql, (input_pid))
+        rows = cur.fetchall()
+        menu_name = rows[0]['name']
+        menu_price = rows[0]['price']
+
+        # 장바구니에 추가
+        sql = "INSERT INTO cart(ord_no, pro_id, qty) VALUES (%s, %s, %s)"
+        cur.execute(sql, (current_oid, input_pid, input_menu_qty))
+
+        # order table의 price 수정
+        sql = "UPDATE orders SET price = price + " + str(menu_price * input_menu_qty) + " WHERE oid = " + str(current_oid)
+        cur.execute(sql)
+
+        sql = "SELECT price FROM orders WHERE oid = " + str(current_oid)
+        cur.execute(sql)
+        rows = cur.fetchall()
+        total_menu_price = rows[0]['price']
+
+
+        # 주문 내역 정리
+        print(menu_name, "을 ", input_menu_qty, "개 선택하였습니다.\n총 금액은 ", menu_price * input_menu_qty, "입니다.")
+        print("장바구니의 총 금액은 ", total_menu_price, "입니다.")
+        print("계속 구매하시겠습니까?")
+        print("1. 계속 구매하기")
+        print("2. 결제하기")
+        cont = int(input(" 번호 선택 >> "))
+
+
+######################################## 에러난다
+
+        # stamp 수, 방문 횟수 증가
+        # stamp 10000원당 1개 적립
+        if (cont == 2):
+            add_stamp = total_menu_price // 10000
+            sql =   " UPDATE customer (no_order, stamp) SET no_order = no_order + 1, stamp = stamp + " + str(add_stamp)
+            cur.execute(sql)
+            rows = cur.fetchall()
+
+            print("결제하기 화면으로 넘어갑니다.")
+            continue_buy = 1
+
+########################################
+
+
+
         
 ### 관리자용 ##############################################
 
@@ -141,6 +254,9 @@ con = pymysql.connect (
 cur = con.cursor()
 
 ### 주문 이전까지의 화면 #################
+
+user_cno = None
+user_name = None
 
 # 첫 화면
 # while (True):
@@ -161,6 +277,7 @@ if (firstpage == 1):
             print("\n", user_name, "님, 반갑습니다!")
         else:                               # 로그인 실패
             print("로그인 실패!!!")
+            # 로그인 실패 시 어떻게..?
 
     else:                           # 유효 어이디 실패 -> 신규회원 가입 페이지로 이동
         print("신규회원 가입 페이지로 이동합니다.")
@@ -172,6 +289,13 @@ elif (firstpage == 2):
     newuser_register()
 
 ### 본격적인 주문하기 ######################################
+
+# 로그인을 실패했을 때 여기로 넘어오지 않게 해야 함
+# print("\n메뉴를 선택해 주세요.")
+# show_menus()
+select_menu(user_cno)
+print("주문이 완료되었습니다.")
+
 
 
 # rows = cur.fetchall()
